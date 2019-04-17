773035a5000799396987262eacb8fdd608b59034
micelle <micelle_yamame@hotmail.co.jp>
2019-04-17 18:13:37 +0900

    [update] 読み込みが遅いときに対応

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
-(() => {
-    'use strict';
-    const body = document.body;
-    const observer = new MutationObserver((mutations) => {
-        const inputs = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
-        if (!inputs) return;
-        inputs.forEach((input) => {
-            input.onmouseover = (e) => {
-                const $this = e.target;
-                const checked = $this.checked;
-                if (window.event.altKey) $this.checked = !checked;
-            }
-        });
+const CheckWithMouseOverStart = () => {
+  const body = document.body;
+  if (!body) {
+    setTimeout(CheckWithMouseOverStart, 500);
+    return;
+  }
+  const observer = new MutationObserver((mutations) => {
+    const inputs = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
+    if (!inputs) return;
+    inputs.forEach((input) => {
+      input.onmouseover = (e) => {
+        const $this = e.target;
+        const checked = $this.checked;
+        if (window.event.altKey) $this.checked = !checked;
+      }
     });
-    observer.observe(body, {
-        childList: true,
-        subtree: true
-    });
-})();
+  });
+  observer.observe(body, {
+    childList: true,
+    subtree: true
+  });
+}
+CheckWithMouseOverStart();

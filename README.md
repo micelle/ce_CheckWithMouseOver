ecc28badb2e93572f835bbfe027b2099720b560f
micelle <micelle_yamame@hotmail.co.jp>
2019-07-12 11:14:14 +0900

    [update] labelタグにも対応

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 CheckWithMouseOver
 =====
 
-チェックボックス・ラジオボタンに対してAltキー＆マウスオーバーでチェックを反転させます。
+![CheckWithMouseOver](https://raw.githubusercontent.com/micelle/ce_CheckWithMouseOver/master/images/webstore/image.gif)
+チェックボックス・ラジオボタンに対してAltキー＆マウスオーバーでチェックを反転させます。ラベルタグにも対応しています。
 
-Invert the check with Alt key & mouse over for check box or radio button.
+Invert the check with Alt key & mouse over for check box or radio button. Also supports label tags.
--- js/index.js
+++ js/index.js
@@ -1,23 +1,50 @@
-const CheckWithMouseOverStart = () => {
+const CheckWithMouseOver = function() {};
+CheckWithMouseOver.prototype.start = () => {
   const body = document.body;
-  if (!body) {
-    setTimeout(CheckWithMouseOverStart, 500);
-    return;
-  }
-  const observer = new MutationObserver((mutations) => {
-    const inputs = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
-    if (!inputs) return;
-    inputs.forEach((input) => {
-      input.onmouseover = (e) => {
-        const $this = e.target;
-        const checked = $this.checked;
-        if (window.event.altKey) $this.checked = !checked;
-      }
-    });
+  if (!body) return setTimeout(CheckWithMouseOver.prototype.start, 500);
+  CheckWithMouseOver.prototype.event();
+  CheckWithMouseOver.prototype.observer(body);
+};
+CheckWithMouseOver.prototype.event = () => {
+  const inputs = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
+  if (inputs) CheckWithMouseOver.prototype.eventInput(inputs);
+  const labels = document.querySelectorAll('label');
+  if (labels) CheckWithMouseOver.prototype.eventLabel(labels);
+};
+CheckWithMouseOver.prototype.eventInput = (inputs) => {
+  inputs.forEach((input) => {
+    input.onmouseover = (e) => {
+      const elm = e.target;
+      CheckWithMouseOver.prototype.checked(elm);
+    }
   });
-  observer.observe(body, {
+};
+CheckWithMouseOver.prototype.eventLabel = (labels) => {
+  labels.forEach((label) => {
+    const attr = label.getAttribute('for');
+    if (attr) {
+      label.onmouseover = (e) => {
+        const elm = document.getElementById(attr);
+        CheckWithMouseOver.prototype.checked(elm);
+      }
+    } else {
+      label.onmouseover = (e) => {
+        const elm = e.target.querySelector('input[type="checkbox"], input[type="radio"]');
+        CheckWithMouseOver.prototype.checked(elm);
+      }
+    }
+  });
+};
+CheckWithMouseOver.prototype.checked = (elm) => {
+  if (elm && window.event.altKey) elm.checked = !elm.checked;
+};
+CheckWithMouseOver.prototype.observer = (elm) => {
+  const observer = new MutationObserver((m) => CheckWithMouseOver.prototype.event());
+  observer.observe(elm, {
     childList: true,
     subtree: true
   });
-}
-CheckWithMouseOverStart();
+};
+window.addEventListener('load', () => {
+  CheckWithMouseOver.prototype.start();
+});

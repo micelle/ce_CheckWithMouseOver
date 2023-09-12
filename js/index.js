436dd13e14febe0fe3883796d046a474a590a250
紅坂柚月 <akasaka@ydk.vc>
2023-09-12 23:12:33 +0900

    [update] iframe内のチェックボックスも対応

--- js/index.js
+++ js/index.js
@@ -1,16 +1,61 @@
 const targetName = 'input[type="checkbox"], input[type="radio"]';
+/**
+ * クロスサイトオリジンか判定
+ * @param {object} frameElm フレーム要素 
+ * @returns ture:同一ホスト, false:クロスサイトオリジン
+ */
+const checkeHost = frameElm => {
+  const hostName = window.location.hostname
+  const frameSrc = frameElm.getAttribute('src')
+  const frameHostName = new URL(frameSrc, window.location.origin).hostname
+  // trueなら同一、falseなら別
+  const isOrigin = new RegExp(frameHostName).test(hostName)
+  // テスト用
+  const test = false
+  if (test) {
+    console.group(frameSrc)
+    console.log('👘 - checkeHost - frameElm:', frameElm)
+    console.log('👘 - checkeHost - hostName:', hostName)
+    console.log('👘 - checkeHost - frameSrc:', frameSrc)
+    console.log('👘 - checkeHost - frameHostName:', frameHostName)
+    console.log('👘 - checkeHost - isOrigin:', isOrigin)
+    console.groupEnd(frameSrc)
+  }
+  // クロスサイトオリジンの場合はすぐに返す
+  if (!isOrigin) return isOrigin
+  // 同一ホストでも Uncaught DOMException: Blocked a frame with origin となるケースもある
+  try {
+    const body = elm.contentWindow.document.body // 試しにbodyを取得
+  } catch (error) {
+    return false // エラーとなる場合はfalseを返す
+  }
+  return isOrigin
+}
 const CheckWithMouseOver = function () { };
 CheckWithMouseOver.prototype.start = () => {
   const body = document.body;
   if (!body) return setTimeout(CheckWithMouseOver.prototype.start, 500);
   CheckWithMouseOver.prototype.event();
   CheckWithMouseOver.prototype.observer(body);
+  // iframe, frame
+  document.querySelectorAll('iframe, frame').forEach(elm => {
+    if (!checkeHost(elm)) return
+    CheckWithMouseOver.prototype.observer(elm.contentWindow.document.body)
+  })
 };
 CheckWithMouseOver.prototype.event = (mutations) => {
   const inputs = document.querySelectorAll(targetName);
   if (inputs) CheckWithMouseOver.prototype.eventInput(inputs);
   const labels = document.querySelectorAll('label');
   if (labels) CheckWithMouseOver.prototype.eventLabel(labels);
+  // iframe, frame
+  document.querySelectorAll('iframe, frame').forEach(elm => {
+    if (!checkeHost(elm)) return
+    const frameInputs = elm.contentWindow.document.querySelectorAll(targetName)
+    if (frameInputs) CheckWithMouseOver.prototype.eventInput(frameInputs)
+    const frameLabels = elm.contentWindow.document.querySelectorAll('label')
+    if (frameLabels) CheckWithMouseOver.prototype.eventLabel(frameLabels)
+  })
 };
 CheckWithMouseOver.prototype.eventInput = (inputs) => {
   inputs.forEach((input) => {
